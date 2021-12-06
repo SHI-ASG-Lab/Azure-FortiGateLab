a87edf9c8a967531836aaa1aae668a4730e97504
j-isley <79328510+j-isley@users.noreply.github.com>
2021-12-06 10:04:05 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,14 +78,14 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_network_security_group" "NSG1" {
-  name                = azurerm_resource_group.main.name"-NSG"
+  name                = "EDR-NSG"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "main" {
-  name                = azurerm_resource_group.main.name"-vNet"
+  name                = "EDR-vNet"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
