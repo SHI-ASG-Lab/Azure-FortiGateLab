8889ae42029922ff52196bccb45f5ae4d6715583
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-12-13 17:38:52 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,34 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.TF_VAR_ARM_SUBSCRIPTION_ID
+  client_id       = var.TF_VAR_ARM_CLIENT_ID
+  client_secret   = var.TF_VAR_ARM_CLIENT_SECRET
+  tenant_id       = var.TF_VAR_ARM_TENANT_ID
 }
 
 
 # Variable Declarations
+
+variable "TF_VAR_ARM_CLIENT_ID" {
+  type = string
+  sensitive = true
+}
+
+variable "TF_VAR_ARM_CLIENT_SECRET" {
+  type = string
+  sensitive = true
+}
+
+variable "TF_VAR_ARM_SUBSCRIPTION_ID" {
+  type = string
+  sensitive = true
+}
+
+variable "TF_VAR_ARM_TENANT_ID" {
+  type = string
+  sensitive = true
+}
 
 variable "region" {
   type = string
@@ -22,44 +46,45 @@ variable "region" {
 variable "RG_Env_Tag" {
     type = string
 }
+
 variable "RG_SP_Name" {
   type = string
 }
+
 variable "Requestor" {
   type = string
-  default = "nil"
+  default = "LAB"
 }
+
 variable "Owner" {
   type = string
-  default = "Morpheus"
+  default = "JWilliams"
 }
 
 variable "Customer" {
   type = string
-  default = "enpro"
-}
-variable "Vendor" {
-  type = string
-  default = "cs"
+  default = "lab"
 }
 
-variable "win10" {
+variable "ubuntu_int" {
   type = number
-  default = 0
-}
-variable "win7" {
-  type = number
-  default = 0
-}
-variable "Ubuntu" {
-  type = number
-  default = 0
-}
-variable "Win19DC" {
-  type = number
-  default = 0
+  default = 1
 }
 
+variable "ubuntu_ext" {
+  type = number
+  default = 1
+}
+
+variable "Win19DC_int" {
+  type = number
+  default = 1
+}
+
+variable "Win19DC_ext" {
+  type = number
+  default = 1
+}
 
 locals {
   common_tags = {
@@ -71,116 +96,212 @@ locals {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "SP-SEC-${var.Customer}-${var.Vendor}"
+  name     = "${var.Customer}-FortiLab-RG"
   location = var.region
 
   tags = local.common_tags
 }
 
-resource "azurerm_network_security_group" "NSG1" {
-  name                = "EDR-NSG"
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "${var.Customer}-FortiLab-nsg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "main" {
-  name                = "EDR-vNet"
+  name                = "${var.Customer}-FortiLab-vnet"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
 }
 
-# Create subnets within the virtual network
-resource "azurerm_subnet" "edrsubnet" {
-    name           = "EDRinternal"
+ Create subnets within the virtual network
+resource "azurerm_subnet" "mgmtsubnet" {
+    name           = var.mgmt_Subnet1_name
     resource_group_name = azurerm_resource_group.main.name
     virtual_network_name = azurerm_virtual_network.main.name
     address_prefixes = ["10.0.1.0/24"]
 }
 
-# Associate Subnets with NSG
-resource "azurerm_subnet_network_security_group_association" "mgmtSubAssocNsg" {
-  subnet_id                 = azurerm_subnet.edrsubnet.id
-  network_security_group_id = azurerm_network_security_group.NSG1.id
+resource "azurerm_subnet" "intsubnet" {
+    name           = var.int_Subnet2_name
+    resource_group_name = azurerm_resource_group.main.name
+    virtual_network_name = azurerm_virtual_network.main.name
+    address_prefixes = ["10.0.2.0/24"]
 }
 
+resource "azurerm_subnet" "extsubnet" {
+    name           = var.ext_Subnet3_name
+    resource_group_name = azurerm_resource_group.main.name
+    virtual_network_name = azurerm_virtual_network.main.name
+    address_prefixes = ["10.0.3.0/24"]
+}
+
+# Associate Subnets with NSG
+resource "azurerm_subnet_network_security_group_association" "mgmtSubAssocNsg" {
+  subnet_id                 = azurerm_subnet.mgmtsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "intSubAssocNsg" {
+  subnet_id                 = azurerm_subnet.intsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "extSubAssocNsg" {
+  subnet_id                 = azurerm_subnet.extsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+# Create Route Tables and specify routes
+resource "azurerm_route_table" "mgmtRtable" {
+  #count = signum(local.Fortinet)
+  name                          = "mgmtRouteTable"
+  location                      = azurerm_resource_group.main.location
+  resource_group_name           = azurerm_resource_group.main.name
+  disable_bgp_route_propagation = true
+
+  route {
+    name           = "mgmt2internal"
+    address_prefix = "10.0.2.0/24"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "10.0.2.4"
+  }
+  route {
+    name           = "mgmt2ext"
+    address_prefix = "10.0.3.0/24"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "10.0.3.4"
+  }
+}
+
+resource "azurerm_route_table" "intRtable" {
+  #count = signum(local.Fortinet)
+  name                          = "intRouteTable"
+  location                      = azurerm_resource_group.main.location
+  resource_group_name           = azurerm_resource_group.main.name
+  disable_bgp_route_propagation = true
+
+  route {
+    name           = "int2mgmt"
+    address_prefix = "10.0.1.0/24"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "10.0.1.4"
+  }
+  route {
+    name           = "int2ext"
+    address_prefix = "10.0.3.0/24"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "10.0.3.4"
+  }
+}
+
+resource "azurerm_route_table" "extRtable" {
+  #count = signum(local.Fortinet + local.Sophos + local.Cisco + local.Juniper + local.PaloAlto + local.Watchguard)
+  name                          = "extRouteTable"
+  location                      = azurerm_resource_group.main.location
+  resource_group_name           = azurerm_resource_group.main.name
+  disable_bgp_route_propagation = true
+
+  route {
+    name           = "ext2internal"
+    address_prefix = "10.0.2.0/24"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "10.0.2.4"
+  }
+  route {
+    name           = "ext2mgmt"
+    address_prefix = "10.0.1.0/24"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "10.0.1.4"
+  }
+}
+
+# Associate Route Tables with Subnets
+resource "azurerm_subnet_route_table_association" "mgmtassoc" {
+  subnet_id      = azurerm_subnet.mgmtsubnet.id
+  route_table_id = azurerm_route_table.mgmtRtable.id
+}
+resource "azurerm_subnet_route_table_association" "intassoc" {
+  subnet_id      = azurerm_subnet.intsubnet.id
+  route_table_id = azurerm_route_table.intRtable.id
+}
+resource "azurerm_subnet_route_table_association" "extassoc" {
+  subnet_id      = azurerm_subnet.extsubnet.id
+  route_table_id = azurerm_route_table.extRtable.id
+}
+
+module "Fortinet" {
+    source = "./modules/FW/Fortinet"
+    #count = local.Fortinet ? 1 : 0 
+
+    resource_group_name = azurerm_resource_group.main.name
+    RGlocation = azurerm_resource_group.main.location
+
+    mgmt_subnet_id     = azurerm_subnet.mgmtsubnet.id
+    int_subnet_id      = azurerm_subnet.intsubnet.id
+    ext_subnet_id      = azurerm_subnet.extsubnet.id 
+
+    tags = local.common_tags
+}
 
 # Add any number of Ubuntu servers
-module "Ubuntu" {
+module "UbuntuINT" {
   source = "./modules/Ubuntu"
-  count = var.Ubuntu
+  count = var.ubuntu_int
 
-  UbuntuVmName = "Ubuntu-${count.index}"
+  UbuntuVmName = "${var.Customer}-FortiLab-Ubuntu-${count.index}"
   
   resource_group_name = azurerm_resource_group.main.name
   RGlocation = azurerm_resource_group.main.location
 
-  subnet_id                 = azurerm_subnet.edrsubnet.id
+  subnet_id                 = azurerm_subnet.intsubnet.id
 
   ipnum = count.index + 20
 
   user = var.Customer
-  pass = "${var.Customer}EDRTest123$"
+  pass = "${var.Customer}SHIisNumber1!"
 
   tags = local.common_tags
 
 }
-# Add in any number of Endpoint Win7 systems from Marketplace as desired
-module "Win7" {
-  source = "./modules/Win7"
-  count = var.win7
 
-  win7vmName = "Win7-${count.index}"
+# Add any number of Ubuntu servers
+module "UbuntuEXT" {
+  source = "./modules/Ubuntu"
+  count = var.ubuntu_ext
+
+  UbuntuVmName = "${var.Customer}-FortiLab-Ubuntu-${count.index}"
   
   resource_group_name = azurerm_resource_group.main.name
   RGlocation = azurerm_resource_group.main.location
 
-  subnet_id                 = azurerm_subnet.edrsubnet.id
+  subnet_id                 = azurerm_subnet.extsubnet.id
+
+  ipnum = count.index + 20
 
   user = var.Customer
-  pass = "${var.Customer}EDRTest123$"
+  pass = "${var.Customer}-FortiLab-${var.Customer}SHIisNumber1!"
 
   tags = local.common_tags
 
-  ipnum = count.index + 40
-  
-}
-# Add in any number of Endpoint Win10 systems from Marketplace as desired
-module "Win10" {
-  source = "./modules/Win10"
-  count = var.win10
-
-  w10vmName = "Win10-${count.index}"
-  
-  resource_group_name = azurerm_resource_group.main.name
-  RGlocation = azurerm_resource_group.main.location
-
-  subnet_id                 = azurerm_subnet.edrsubnet.id
-
-  user = var.Customer
-  pass = "${var.Customer}EDRTest123$"
-
-  tags = local.common_tags
-
-  ipnum = count.index + 10
-  
 }
 
 # Add in any number of "Windows 2019 Datacenter" Servers
-module "win19" {
+module "win19int" {
   source = "./modules/win19"
-  count = var.Win19DC
+  count = var.Win19DC_int
 
-  VmName = "Win19-${count.index}"
+  VmName = "${var.Customer}-FortiLab-Win19-${count.index}"
   
   resource_group_name = azurerm_resource_group.main.name
   RGlocation = azurerm_resource_group.main.location
 
-  subnet_id                 = azurerm_subnet.edrsubnet.id
+  subnet_id                 = azurerm_subnet.intsubnet.id
 
   user = var.Customer
-  pass = "${var.Customer}EDRTest123$"
+  pass = "${var.Customer}SHIisNumber1!"
 
   tags = local.common_tags
 
@@ -188,44 +309,23 @@ module "win19" {
   
 }
 
-# Win10 Jumpbox from Marketplace
-module "Win10JumpBox" {
-  source = "./modules/Win10JumpBox"
+ Add in any number of "Windows 2019 Datacenter" Servers
+module "win19ext" {
+  source = "./modules/win19"
+  count = var.Win19DC_ext
 
-  w10vmName  = "Win10-Jumpbox"
+  VmName = "${var.Customer}-FortiLab-Win19-${count.index}"
   
   resource_group_name = azurerm_resource_group.main.name
   RGlocation = azurerm_resource_group.main.location
 
-  subnet_id                 = azurerm_subnet.edrsubnet.id
-
-  DNS_Name = "${var.Customer}${var.Vendor}jump"
+  subnet_id                 = azurerm_subnet.extsubnet.id
 
   user = var.Customer
-  pass = "${var.Customer}EDRTest123$"
+  pass = "${var.Customer}SHIisNumber1!"
 
   tags = local.common_tags
 
-  ipnum = 5
-  
-}
-
-# Kali Attacker from Marketplace
-module "Kali" {
-  source = "./modules/Kali"
-
-  kaliVmName = "Kali-Attacker"
-  
-  resource_group_name = azurerm_resource_group.main.name
-  RGlocation = azurerm_resource_group.main.location
-
-  subnet_id                 = azurerm_subnet.edrsubnet.id
-
-  user = var.Customer
-  pass = "${var.Customer}EDRTest123$"
-
-  tags = local.common_tags
-
-  ipnum = 4
+  ipnum = count.index + 30
   
 }
