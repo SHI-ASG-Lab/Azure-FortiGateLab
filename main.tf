a52ce3ce938519c1f16718e60507c17aebd75c18
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-12-14 15:34:49 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -259,7 +259,7 @@ module "Fortinet" {
     resource_group_name = azurerm_resource_group.main.name
     RGlocation = azurerm_resource_group.main.location
  
-    customer_id        = azurerm_
+    customer_id        = var.Customer
     mgmt_subnet_id     = azurerm_subnet.mgmtsubnet.id
     int_subnet_id      = azurerm_subnet.intsubnet.id
     ext_subnet_id      = azurerm_subnet.extsubnet.id 
