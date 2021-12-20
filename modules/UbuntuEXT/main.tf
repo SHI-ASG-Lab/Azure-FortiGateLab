e3a161061aefac9ff1f48d7d78b353c52656261f
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-12-20 16:53:20 -0600

    Update main.tf

--- modules/UbuntuEXT/main.tf
+++ modules/UbuntuEXT/main.tf
@@ -50,7 +50,13 @@ resource "azurerm_virtual_machine" "main" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
+  
+  plan {
+    name      = "0001-com-ubuntu-pro-focal"
+    publisher = "canonical"
+    product   = "pro-20_04-lts"
+  }
+  
   tags     = var.tags
 }
 
