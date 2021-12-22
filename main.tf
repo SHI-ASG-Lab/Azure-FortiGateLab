0f6f0c9c48ab2bf93a8d1ec28ffc9c8dd257ab1a
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-12-21 18:03:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -138,6 +138,21 @@ resource "azurerm_network_security_group" "nsg1" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
+# Create NSG rules
+
+resource "azurerm_network_security_rule" "ngsrule" {
+  name                        = "ANY"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg1.name
+
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "main" {
   name                = "${var.Customer}-FortiLab-vnet"
