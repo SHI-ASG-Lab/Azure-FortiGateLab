6c5706c06798735c2273f5821697a89dc8acbb15
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-12-21 18:20:59 -0600

    Update main.tf

--- modules/UbuntuINT/main.tf
+++ modules/UbuntuINT/main.tf
@@ -26,6 +26,7 @@ resource "azurerm_network_interface" "main" {
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "static"
     private_ip_address = "10.0.2.${var.ipnum}"
+    public_ip_address_id = azurerm_public_ip.azPubIp.id
     primary = true
   }
 }
