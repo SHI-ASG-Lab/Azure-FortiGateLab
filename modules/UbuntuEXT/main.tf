bca57c23096997cebe3b1aa806b676d76f7847be
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-12-21 18:13:23 -0600

    Update main.tf

--- modules/UbuntuEXT/main.tf
+++ modules/UbuntuEXT/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_public_ip" "azPubIp" {
 }
 
 # Create NIC for the VM
-resource "azurerm_network_interface" "pubip" {
+resource "azurerm_network_interface" "main" {
   name                = "${var.UbuntuName}-nic"
   location            = var.RGlocation
   resource_group_name = var.resource_group_name
